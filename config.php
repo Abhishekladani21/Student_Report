80c313084ceac85bbc5820047e682a1fd2cd0fb3
Abhi <abhiladani007@gmail.com>
2023-12-12 11:57:54 +0530

    db connection 12-12

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-$servername = "localhost";
+$hostname = "localhost";
 
 $username = "root"; 
 
@@ -8,12 +8,29 @@ $password = "";
 
 $dbname = "studentform"; 
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-
+$conn = new mysqli($hostname, $username, $password);
+if ($conn->connect_error){
     die("Connection failed: " . $conn->connect_error);
-
 }
 
+$sql = "CREATE DATABASE IF NOT EXISTS $dbname";
+if ($conn->query($sql) === FALSE){
+    die("Error creating database: " . $conn->error);
+}
+
+$conn->select_db($dbname);
+
+$sql = "CREATE TABLE IF NOT EXISTS users (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `studentName` varchar(255) NOT NULL,
+    `rollNo` int(10) NOT NULL,
+    `standard` varchar(200) NOT NULL,
+    `dob` date NOT NULL,
+    `contactNumber` varchar(255) NOT NULL,
+    `address` varchar(200) NOT NULL
+    )";
+    
+    if ($conn->query($sql) === FALSE){
+        die("Error creating table: " . $conn->error);
+    }
 ?> 
---
+++ db/studentform.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 12, 2023 at 07:22 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `studentform`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) NOT NULL,
+  `studentName` varchar(255) NOT NULL,
+  `rollNo` int(10) NOT NULL,
+  `standard` varchar(200) NOT NULL,
+  `dob` date NOT NULL,
+  `contactNumber` varchar(255) NOT NULL,
+  `address` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `studentName`, `rollNo`, `standard`, `dob`, `contactNumber`, `address`) VALUES
+(3, 'Abhi ladani', 302, 'B.com', '2002-08-21', '9574424478', 'surat'),
+(12, 'kevin', 2, 'bca', '2003-11-11', '123456789', 'surat');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
